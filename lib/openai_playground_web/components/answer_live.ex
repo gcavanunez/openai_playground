84141299ccfe3446a4e978e0df956e9a2e43a925
Guillermo Cava <gcavanunez@gmail.com>
2023-03-04 15:53:05 -0500

    working state

--- lib/openai_playground_web/components/answer_live.ex
+++ lib/openai_playground_web/components/answer_live.ex
@@ -17,7 +17,7 @@ defmodule OpenaiPlaygroundWeb.AnswerLive do
   defp stream_response(stream) do
     target = self()
 
-    Task.Supervisor.async(StreamingText.TaskSupervisor, fn ->
+    Task.Supervisor.async(OpenaiPlayground.TaskSupervisor, fn ->
       for chunk <- stream, into: <<>> do
         send(target, {:render_response_chunk, chunk})
         chunk
@@ -26,7 +26,7 @@ defmodule OpenaiPlaygroundWeb.AnswerLive do
   end
 
   @impl true
-  def handle_event("answer_question", %{"question" => %{"question" => question}}, socket) do
+  def handle_event("answer_question", %{"question" => question}, socket) do
     prompt = prompt(question)
     stream = OpenaiPlayground.OpenAI.stream(prompt)
 
@@ -53,12 +53,13 @@ defmodule OpenaiPlaygroundWeb.AnswerLive do
     <div class="flex flex-col max-w-4xl min-h-screen items-center">
       <h1 class="text-2xl">Ask Me Anything</h1>
       <.simple_form :let={f} for={%{}} as={:question} phx-submit="answer_question" class="w-full">
-        <%!-- <.input
-          disabled={@state != :waiting_for_question}
-          field={{f, :question}}
-          value={@question}
-          type="text"
-        /> --%>
+        <%!-- field={{f, :question}}
+          value={@question} disabled={@state != :waiting_for_question}  --%>
+        <%!-- <input type="text" /> --%>
+        <%!-- <.input name="some_input" /> --%>
+        <%!-- <.input field={@form[:email]} label="Email"/> --%>
+        <%!-- disabled={@state != :waiting_for_question} --%>
+        <.input name="question" value={@question} label="Username" />
         <.button
           type="submit"
           disabled={@state != :waiting_for_question}
@@ -73,5 +74,24 @@ defmodule OpenaiPlaygroundWeb.AnswerLive do
       </div>
     </div>
     """
+  end
+
+  @impl true
+  def handle_info({:render_response_chunk, chunk}, socket) do
+    answer = socket.assigns.answer <> chunk
+    {:noreply, assign(socket, :answer, answer)}
+  end
+
+  def handle_info({ref, answer}, socket) when socket.assigns.response_task.ref == ref do
+    socket =
+      socket
+      |> assign(:answer, answer)
+      |> assign(:state, :waiting_for_question)
+
+    {:noreply, socket}
+  end
+
+  def handle_info(_message, socket) do
+    {:noreply, socket}
   end
 end
